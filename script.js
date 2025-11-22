4323a6cbc2a85498eddd2560f3b495b7196c9f36
Pratik Mane <manepratik3727@gmail.com>
2025-11-22 22:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const submit=document.querySelector("input[type=submit]").click()
 function getFormvalue(){
-	const john=document.querySelector(".fname").type("John")
+	const john=document.querySelector(".fname")
 	const doe=document.querySelector(".lname").type("Mark Doe")
 
 	return alert(john+" "+doe)
