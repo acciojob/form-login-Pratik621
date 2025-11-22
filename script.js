b72137c70926fd470360a26c2024a8e047d4fa33
Pratik Mane <manepratik3727@gmail.com>
2025-11-22 19:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
+
+const submit=document.querSelector("input[type=submit]")
+
+submit.addEventListener("click",()=>{
+	   getFormvalue()
+})
 function getFormvalue() {
-    const fname = document.querySelector('input[name="fname"]').value.trim();
-    const lname = document.querySelector('input[name="lname"]').value.trim();
-
-    // Use the string directly, do NOT use .value
-    alert(`${fname} ${lname}`);
-
-    // Clear inputs after alert
-    document.querySelector('input[name="fname"]').value = '';
-    document.querySelector('input[name="lname"]').value = '';
+	const fname=document.querySelctor(".fname")
+const lname=document.querySelctor(".lname")
+	return alert(`${fname.value} ${lname.value}`)
+	
+	
 }
-
-document.getElementById("form1").addEventListener("submit", function(e) {
-    e.preventDefault();  // Prevent form submission/reload
-    getFormvalue();
-});
