033f76f9945b5561913aaa65de6154226b9ff18c
Pratik Mane <manepratik3727@gmail.com>
2025-11-22 22:14:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-function showAlert() {
-  const fname = document.querySelector('input[name="fname"]').value;
-  const lname = document.querySelector('input[name="lname"]').value;
-
-  alert(fname + " " + lname);
-}
 const submit=document.querySelector("input[type=submit]")
-submit.addEventListener("click",()=>{
-	("onclick",showAlert())
-})
+function getFormvalue(){
+	const john=document.querySelector(".fname").value
+	const doe=document.querySelector(".lname").value
+
+	return alert(john+" "+doe)
+}
