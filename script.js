a64701fd71a9a97ed9ed0aeb90762b063d9a6a5d
Pratik Mane <manepratik3727@gmail.com>
2025-11-22 12:52:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const submit=document.querySelector("button");
 function getFormvalue() {
     //Write your code here
+	 alert(`${fname.value} ${lname.value}`)
+
 
 }
+submit.addEventListener("click",()=>{
+    const fname=document.querySelector("input[type=fname]")
+const lname=document.querySelector("input[type=lname]")
+    getFormvalue(fname,lname);
+})
